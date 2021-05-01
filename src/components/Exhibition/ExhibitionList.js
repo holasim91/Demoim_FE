d281b6d6708c68bbfde9a050ab228cafd9e64cd4
HyunIn Sim <>
2021-05-01 10:01:48 +0900

    자랑하기 PostListWrapper 위치 수정

--- src/components/Exhibition/ExhibitionList.js
+++ src/components/Exhibition/ExhibitionList.js
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ExhibitionPost from "./ExhibitionPost";
 import { actionCreators as exhibitionActions } from "../../redux/modules/exhibition";
 import { history } from "../../redux/configStore";
+import styled from "styled-components";
 
 const ExhibitionList = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [page, setPage] = useState(1); //현재페이지, 1부터 시작
-  useEffect(()=>{
-    dispatch(exhibitionActions.exihibitionAPI(page, 6))
-  },[dispatch,page])
-const {posts, isLoading} = useSelector((state) => state.exhibition)
+  useEffect(() => {
+    dispatch(exhibitionActions.exihibitionAPI(page, 6));
+  }, [dispatch, page]);
+  const { posts, isLoading } = useSelector((state) => state.exhibition);
 
-if(isLoading){
-  return(
-    <>
-      로오딩중
-    </>
-  )
-}
-
+  if (isLoading) {
+    return <>로오딩중</>;
+  }
 
   return (
-    <>
+    <ExhibitionBoxWrapper>
       {posts.map((post) => (
-            <ExhibitionPost data={post} key={post.id} onClick={()=>history.push(`/detail/${post.id}`)} />
-          ))}
+        <ExhibitionPost
+          data={post}
+          key={post.id}
+          onClick={() => history.push(`/detail/${post.id}`)}
+        />
+      ))}
+    </ExhibitionBoxWrapper>
+  );
+};
 
-    </>
-  )
-}
+const ExhibitionBoxWrapper = styled.div`
+  margin: 30px auto 0 auto;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 2%;
+  grid-row-gap: 27px;
 
-export default ExhibitionList
+  @media ${(props) => props.theme.tablet} {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 0%;
+  }
+`;
+
+export default ExhibitionList;
--- src/pages/Exhibition.js
+++ src/pages/Exhibition.js
@@ -7,35 +7,14 @@ import ExhibitionList from "../components/Exhibition/ExhibitionList";
 const Exhibition = (props) => {
   return (
     <>
-    <SubMenus currentPath = {props.location.pathname.split('/')[1]}/>
-    
+      <SubMenus currentPath={props.location.pathname.split("/")[1]} />
+
       <Container>
         <Grid padding="100px 0 0 0" />
-        <ExhibitionBoxWrapper>
-          <ExhibitionList />
-        </ExhibitionBoxWrapper>
+        <ExhibitionList />
       </Container>
     </>
   );
 };
-
-const ExhibitionBoxWrapper = styled.div`
-  margin:30px auto 0 auto;
-  width:100%;
-  display: grid;
-  grid-template-columns: repeat(2,1fr);
-  grid-column-gap: 2%;
-  grid-row-gap: 27px;
-
-  @media ${props => props.theme.tablet}{
-  grid-template-columns: repeat(2,minmax(0,1fr));
-  }
-
-  @media ${props => props.theme.mobile}{
-    grid-template-columns: repeat(1,1fr);
-   grid-gap:0%;
- }
-`;
-
 
 export default Exhibition;
