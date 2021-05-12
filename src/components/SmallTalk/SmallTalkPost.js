efa8bfcd7246e35cfc9958ac5e550d688cf63c15
HyunIn Sim <>
2021-05-13 02:50:29 +0900

    디폴트 이미지 빠진거 추가

--- src/components/SmallTalk/SmallTalkPost.js
+++ src/components/SmallTalk/SmallTalkPost.js
@@ -95,7 +95,7 @@ const SmallTalkPost = (props) => {
               <ProfileImage
                 alt="profile"
                 src={
-                  "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                  DefaultProfile
                 }
               />
             )}
